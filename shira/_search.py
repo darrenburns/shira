46652f6bc62f315d566aede35b788a6bc458b3d2
Darren Burns <darrenb900@gmail.com>
2022-11-11 12:13:24 +0000

    Loading module info into body

--- shira/_search.py
+++ shira/_search.py
@@ -7,38 +7,38 @@ from typing import Iterable
 from rich.console import RenderableType, Console, ConsoleOptions
 from rich.text import Text
 from textual import events
-from textual.app import ComposeResult
 from textual.css.styles import RenderStyles
-from textual.geometry import Size
+from textual.geometry import Size, Region
+from textual.message import Message
 from textual.widget import Widget
 from textual.widgets import Input
 
 
 class SearchCompletionRender:
-    def __init__(self, filter: str, matches: Iterable[pkgutil.ModuleInfo], highlight_index: int, component_styles: dict[str, RenderStyles]) -> None:
+    def __init__(self, filter: str, matches: Iterable[pkgutil.ModuleInfo],
+                 highlight_index: int,
+                 component_styles: dict[str, RenderStyles]) -> None:
         self.filter = filter
         self.matches = matches
         self.highlight_index = highlight_index
         self.component_styles = component_styles
-        self._highlight_item_style = self.component_styles.get("search-completion--selected-item").rich_style
+        self._highlight_item_style = self.component_styles.get(
+            "search-completion--selected-item").rich_style
 
     def __rich_console__(self, console: Console, options: ConsoleOptions):
         matches = []
-        for match in self.matches:
-            match = Text(f"{match.name:<{console.width}}", style="italic" if match.ispkg else "")
+        for index, match in enumerate(self.matches):
+            match = Text.from_markup(
+                f"{match.name:<{options.max_width - 3}}[dim]{'pkg' if match.ispkg else 'mod'}")
             matches.append(match)
-
-        sorted_matches = sorted(matches, key=lambda string: not string.plain.startswith(self.filter))
-        for index, match in enumerate(sorted_matches):
             if self.highlight_index == index:
                 match.stylize(self._highlight_item_style)
             match.highlight_regex(self.filter, style="black on #4EBF71")
 
-        return Text("\n").join(sorted_matches)
+        return Text("\n").join(matches)
 
 
 class SearchCompletion(Widget):
-
     COMPONENT_CLASSES = {
         "search-completion--selected-item",
     }
@@ -52,6 +52,12 @@ class SearchCompletion(Widget):
     ):
         super().__init__(name=name, id=id, classes=classes)
         self.modules = sorted(modules, key=attrgetter("name"))
+
+    @property
+    def highlighted_module(self):
+        if not self.matches:
+            return 0
+        return self.matches[self.highlight_index]
 
     @property
     def highlight_index(self) -> int:
@@ -69,14 +75,18 @@ class SearchCompletion(Widget):
     @search_value.setter
     def search_value(self, value: str):
         self._search_value = value
-        self.matches = [module for module in self.modules if value in module.name]
+        new_matches = [module for module in self.modules if value in module.name]
+        self.matches = sorted(new_matches,
+                              key=lambda module: module.name.startswith(value),
+                              reverse=True)
         self.parent.display = len(value) > 1 and len(self.matches) > 0
-        self.refresh(layout=True)
+        self.refresh()
 
     def on_mount(self, event: events.Mount) -> None:
         self._highlight_index = 0
         self._search_value = ""
-        self.matches = [module for module in self.modules]
+        self.matches = sorted([module for module in self.modules],
+                              key=attrgetter("name"))
 
     def get_content_width(self, container: Size, viewport: Size) -> int:
         if not self.matches:
@@ -97,24 +107,26 @@ class SearchCompletion(Widget):
 
 
 class SearchBar(Input):
-    def compose(self) -> ComposeResult:
-        yield Input(
-            value="",
+
+    def __init__(self, name: str | None = None,
+                 id: str | None = None,
+                 classes: str | None = None):
+        super().__init__(
             placeholder="Start typing to search...",
-            id="search-input",
+            name=name,
+            id=id,
+            classes=classes,
         )
 
-    def on_input_changed(self, event: Input.Changed) -> None:
-        value = event.value
+    def watch_value(self, value: str) -> None:
         completion = self.app.query_one(SearchCompletion)
         completion_parent = self.app.query_one("#search-completion-container")
-        input = self.query_one("#search-input", Input)
         top, right, bottom, left = completion_parent.styles.margin
         completion_parent.styles.margin = (
             top,
             right,
             bottom,
-            input.cursor_position + 3
+            self.cursor_position + 3
         )
         completion.search_value = value
         # Trigger the property setter to ensure validation re-runs.
@@ -122,9 +134,36 @@ class SearchBar(Input):
 
     def on_key(self, event: events.Key) -> None:
         completion = self.app.query_one(SearchCompletion)
+        completion_parent = self.app.query_one("#search-completion-container")
+
+        # TODO: Scroll the completion container appropriately
         if event.key == "down":
             completion.highlight_index += 1
+            event.stop()
         elif event.key == "up":
             completion.highlight_index -= 1
+            event.stop()
+        elif event.key == "tab":
+            if completion_parent.display:
+                # The dropdown is visible, fill in the completion string
+                module = completion.highlighted_module
+                value = module.name + " "
+                self.value = value
+                self.cursor_position = len(value)
+                completion.highlight_index = completion.highlight_index
+                completion_parent.display = False
 
+                self.emit_no_wait(self.NewLookupChain(self, [module]))
 
+                event.stop()
+
+        # TODO: More sensible scrolling
+        x, y, width, height = completion.region
+        target_region = Region(x, completion.highlight_index, width, height)
+        completion_parent.scroll_to_region(target_region, animate=False)
+
+    class NewLookupChain(Message, bubble=True):
+        def __init__(self, sender: SearchBar,
+                     chain: list[str | pkgutil.ModuleInfo]) -> None:
+            super().__init__(sender)
+            self.chain = chain
--- shira/shira.py
+++ shira/shira.py
@@ -3,8 +3,9 @@ import pkgutil
 from textual import events
 from textual.app import App, ComposeResult
 from textual.containers import Container, Horizontal
-from textual.widgets import Static, Input
+from textual.widgets import Static
 
+from shira._module_panel import ModulePanel
 from shira._search import SearchBar, SearchCompletion
 
 
@@ -13,7 +14,7 @@ class Shira(App):
         modules = pkgutil.iter_modules()
         yield Horizontal(
             Static(">", id="search-prompt"),
-            SearchBar(id="search-bar"),
+            SearchBar(id="search-input"),
             id="search-bar-container",
         )
         yield Container(
@@ -24,15 +25,19 @@ class Shira(App):
             id="search-completion-container",
         )
         yield Container(
-            Static("Body content"),
+            ModulePanel(id="module-panel"),
             id="body-container",
         )
 
     def on_mount(self, event: events.Mount) -> None:
         self.query_one("#search-input").focus()
 
-    def on_input_changed(self, event: Input.Changed) -> None:
-        value = event.value
+    def on_search_bar_new_lookup_chain(self, event: SearchBar.NewLookupChain) -> None:
+        module_panel = self.query_one("#module-panel", ModulePanel)
+        self.query_one("#body-container").scroll_home(animate=False)
+
+        new_module = event.chain[0] if event.chain else None
+        module_panel.active_module = new_module
 
 
 app = Shira(css_path="shira.scss")
